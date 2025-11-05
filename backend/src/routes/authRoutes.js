5cf3a11340b190632941978f69480c47d82d1d8f
anuj.mishra191 <anuj.mishra420b6r9@gmail.com>
2025-11-05 19:58:49 +0530

    try to fix createProductType

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -20,7 +20,7 @@ router.post("/adminSignup",authenticate,checkMaster, adminSignup);
 router.post("/login", login);
 router.post("/logout", logout);
 router.post("/createCategory",authenticate,checkMaster, CategoryRoute);
-router.post("/createProductType",authenticate,checkMaster, ProductTypeRoute);
+router.post("/createProductType",authenticate, ProductTypeRoute);
 router.post("/productCreate",  authenticate,checkAdmin, upload.fields([{ name: "productFiles", maxCount: 3 }]),productCreate);export default router;
 router.post("/cart", authenticate ,cart);
 router.post("/orderHistory",authenticate,createOrderHistory);
