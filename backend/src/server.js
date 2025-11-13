971c48a6988d5407880d3a6beb125d72a4163d34
anuj.mishra191 <anuj.mishra420b6r9@gmail.com>
2025-11-13 23:54:32 +0530

    upadate server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -20,7 +20,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: ["https://thokmarket-frontend.onrender.com"],
+    origin: ["https://thokmarket-191.onrender.com"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
