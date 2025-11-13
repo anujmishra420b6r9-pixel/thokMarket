8939caafdf5f6f059207e6a5add48fed9d745bca
anuj.mishra191 <anuj.mishra420b6r9@gmail.com>
2025-11-13 23:59:40 +0530

    allow both backend links

--- backend/src/server.js
+++ backend/src/server.js
@@ -20,7 +20,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: ["https://thokmarket-191.onrender.com"],
+    origin: ["https://thokmarket-191.onrender.com", "https://thokmarket.onrender.com"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
